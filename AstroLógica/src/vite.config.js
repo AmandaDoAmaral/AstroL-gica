02f32afaaf0e31df1de6887672fe763bcc76256c
Amanda Maria Baumgartner Do Amaral <134961844+AmandaDoAmaral@users.noreply.github.com>
2026-05-25 20:32:19 -0300

    Update vite.config.js

--- AstroLógica/src/vite.config.js
+++ AstroLógica/src/vite.config.js
@@ -1,9 +1,7 @@
-import base44 from "@base44/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
 
-// https://vite.dev/config/
 export default defineConfig({
   logLevel: "error",
 
@@ -13,14 +11,5 @@ export default defineConfig({
     }
   },
 
-  plugins: [
-    base44({
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === "true",
-      hmrNotifier: true,
-      navigationNotifier: true,
-      analyticsTracker: true,
-      visualEditAgent: true
-    }),
-    react()
-  ]
+  plugins: [react()]
 });
